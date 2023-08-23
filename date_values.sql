e9b1fb1bef73ad5e392ec29847cbece4a75221f5
Ænðr E. Feldstraw <aev@sdf.org>
2023-08-23 00:17:04 -0400

    Added foreign key on attribute id

--- date_values.sql
+++ date_values.sql
@@ -5,5 +5,6 @@ CREATE TABLE `date_values` (
  PRIMARY KEY (`attribute_id`,`case_id`),
  KEY `value` (`value`),
  KEY `fk_1` (`case_id`),
- CONSTRAINT `fk_1` FOREIGN KEY (`case_id`) REFERENCES `profiles` (`case_id`) ON DELETE CASCADE ON UPDATE CASCADE
+ CONSTRAINT `fk_1` FOREIGN KEY (`case_id`) REFERENCES `profiles` (`case_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `fk_7` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Values for text attributes'
