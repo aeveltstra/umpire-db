edaddc237be3e91389bf2410a9072527d10b0d70
Ænðr E. Feldstraw <aev@sdf.org>
2023-08-22 22:59:07 -0400

    Superfluous key removed

--- date_values.sql
+++ date_values.sql
@@ -3,6 +3,5 @@ CREATE TABLE `date_values` (
  `case_id` int(4) NOT NULL,
  `value` date NOT NULL,
  PRIMARY KEY (`attribute_id`,`case_id`),
- KEY `fk_case_id` (`case_id`),
  KEY `value` (`value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Values for text attributes'
