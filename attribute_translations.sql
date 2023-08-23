b4f22f23a41bc3e61d0322700822e301f3687bf3
Ænðr E. Feldstraw <aev@sdf.org>
2023-08-22 23:22:22 -0400

    Added the foreign key

--- attribute_translations.sql
+++ attribute_translations.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `attribute_translations` (
-  `language_code` varchar(24) CHARACTER SET ascii NOT NULL,
-  `attribute_id` char(24) NOT NULL,
-  `translation` text NOT NULL,
-  PRIMARY KEY (`language_code`,`attribute_id`),
-  KEY `fk_attribute_id` (`attribute_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Translations of attribute captions'	
+ `language_code` varchar(24) CHARACTER SET ascii NOT NULL,
+ `attribute_id` char(24) NOT NULL,
+ `translation` text NOT NULL,
+ PRIMARY KEY (`language_code`,`attribute_id`),
+ KEY `fk_attrib_id` (`attribute_id`),
+ CONSTRAINT `fk_attrib_id` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Translations of attribute captions'
