9995174ffc2a658e9c4d83a8119806c7d2889a6c
Œnðr <aev@sdf.org>
2023-09-06 19:21:28 -0400

    added attributes

---
+++ data/attributes-data.sql
@@ -0,0 +1,42 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+INSERT INTO `attributes` (`id`, `data_type`, `display_sequence`) VALUES
+('aliases', 'shorttext', 0),
+('birth year', 'integer', 0),
+('birth year accuracy', 'integer', 0),
+('case id', 'shorttext', 0),
+('case status', 'enum', 0),
+('chosen name', 'shorttext', 0),
+('contact if found', 'shorttext', 0),
+('date calendar type', 'enum', 0),
+('disappear circumstances', 'shorttext', 0),
+('entered by', 'enum', 0),
+('entered on date', 'date', 0),
+('ethnicity', 'enum', 0),
+('eye color', 'shorttext', 0),
+('eye color std', 'enum', 0),
+('given name', 'shorttext', 0),
+('hair color', 'shorttext', 0),
+('hair color std', 'enum', 0),
+('height', 'integer', 0),
+('height unit', 'enum', 0),
+('identifying features', 'shorttext', 0),
+('last modified by', 'enum', 0),
+('last modified on date', 'date', 0),
+('last seen activity', 'shorttext', 0),
+('last seen at location', 'shorttext', 0),
+('last seen date accuracy', 'integer', 0),
+('last seen on date', 'date', 0),
+('last seen wearing', 'shorttext', 0),
+('other notes', 'shorttext', 0),
+('posters generated on', 'date', 0),
+('pronouns', 'shorttext', 0),
+('weight', 'integer', 0),
+('weight unit', 'enum', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
