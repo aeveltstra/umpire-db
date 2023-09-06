c2cfc256bec9aff44dd97ba671b02b143daa362e
Œnðr <aev@sdf.org>
2023-09-06 19:18:42 -0400

    separated model from data

---
+++ data/enums-data.sql
@@ -0,0 +1,58 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+INSERT INTO `enums` (`attribute_id`, `enum_value`) VALUES
+('case status', 'solved'),
+('case status', 'unsolved'),
+('ethnicity', 'R1: American Indian or A'),
+('ethnicity', 'R2: Asian'),
+('ethnicity', 'R3: Black or African Ame'),
+('ethnicity', 'R4: Native Hawaiian or O'),
+('ethnicity', 'R5: White'),
+('ethnicity', 'R9: Other'),
+('ethnicity', 'unknown'),
+('eye color std', 'black'),
+('eye color std', 'blue'),
+('eye color std', 'brown'),
+('eye color std', 'green'),
+('eye color std', 'grey'),
+('eye color std', 'hazel'),
+('eye color std', 'multiple'),
+('eye color std', 'red'),
+('eye color std', 'unknown'),
+('hair color std', 'black'),
+('hair color std', 'blonde'),
+('hair color std', 'blue'),
+('hair color std', 'brown'),
+('hair color std', 'dirty blonde'),
+('hair color std', 'ginger'),
+('hair color std', 'green'),
+('hair color std', 'grey'),
+('hair color std', 'multiple'),
+('hair color std', 'pink'),
+('hair color std', 'platinum'),
+('hair color std', 'purple'),
+('hair color std', 'red'),
+('hair color std', 'unknown'),
+('hair color std', 'white'),
+('height unit', 'centimeter'),
+('height unit', 'inches'),
+('publication channel', 'discord'),
+('publication channel', 'facebook'),
+('publication channel', 'flyer (print)'),
+('publication channel', 'friendica'),
+('publication channel', 'instagram'),
+('publication channel', 'mastodon'),
+('publication channel', 'newspaper'),
+('publication channel', 'public news release'),
+('publication channel', 'reddit'),
+('publication channel', 'tdtf wiki'),
+('publication channel', 'twitter'),
+('weight unit', 'kilograms'),
+('weight unit', 'lbs');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
