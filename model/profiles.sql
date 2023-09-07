8d6d1d9bb3687222e5942541651a1c6fe24dcf02
Œnðr <aev@sdf.org>
2023-09-06 20:00:27 -0400

    Added profiles table

---
+++ model/profiles.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `profiles` (
+ `case_id` bigint(20) NOT NULL AUTO_INCREMENT,
+ PRIMARY KEY (`case_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Cases of unidentified and
+missing people';
+
