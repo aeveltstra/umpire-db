1245c9edda07fddac8f815b30eacbd621400b7a9
Œnðr <aev@sdf.org>
2023-09-09 23:47:03 -0400

    allowing for credentials reset, and making the user's email into the record key

--- model/users.sql
+++ model/users.sql
@@ -1,18 +1,20 @@
 CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'just a sequence nr.',
-  `key_hash` char(24) NOT NULL,
-  `secret_hash` char(24) NOT NULL,
+  `email_hash` char(24) NOT NULL,
   `display_name` varchar(256),
-  `email` varchar(512),
-  `encryption_algo` varchar(128),
-  `encryption_version` varchar(24),
-  `last_encrypted` date NOT NULL,
-  `requested_access_on` date NOT NULL,
-  PRIMARY KEY (`id`),
+  `access_requested_on` date,
+  `access_approved_on` date,
+  `key_hash` char(24),
+  `secret_hash` char(24),
+  `hashing_algo` varchar(128),
+  `hashing_version` varchar(24),
+  `last_hashed_date` date,
+  `reset_key` char(24),
+  `reset_key_date` date,
+  PRIMARY KEY (`email_hash`),
   KEY `token` (`key_hash`, `secret_hash`),
-  KEY `encryption_algo` (`encryption_algo`),
-  KEY `last_encrypted` (`last_encrypted`),
-  KEY `email` (`email`),
-  KEY `requested_access_on` (`requested_access_on`)
+  KEY `hashing` (`hashing_algo`, `hashing_version`),
+  KEY `last_hashed` (`last_hashed`),
+  KEY `access_requested_on` (`access_requested_on`),
+  KEY `reset_key` (`reset_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='People allowed to use the system';
 
