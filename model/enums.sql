9a68c9846e2fc485376c9020f01741c6427a6809
Ænðr E. Feldstraw <aev@sdf.org>
2023-09-19 18:28:02 -0400

    Added language field
    and some foreign keys for data consistency

--- model/enums.sql
+++ model/enums.sql
@@ -1,5 +1,10 @@
 CREATE TABLE `enums` (
  `attribute_id` char(24) NOT NULL,
- `enum_value` char(24) NOT NULL,
- PRIMARY KEY (`attribute_id`,`enum_value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='All possible enumerations for all attributes';
+ `enum_value` varchar(24) NOT NULL,
+ `language_code` varchar(24) CHARACTER SET ascii NOT NULL,
+ PRIMARY KEY (`attribute_id`,`enum_value`,`language_code`) USING BTREE,
+ KEY `ix_lang` (`language_code`),
+ KEY `ix_attrib` (`attribute_id`) USING BTREE,
+ CONSTRAINT `fk_enums_attrib_attributes` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `fk_enums_lang_languages` FOREIGN KEY (`language_code`) REFERENCES `language_codes` (`code`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='All possible enumerations for all attributes'
