d555dcf41303557811cc410bf49fb56089efe3c9
Œnðr <aev@sdf.org>
2023-11-18 13:34:15 -0500

    Added form caption translations

---
+++ data/form_caption_translations.sql
@@ -0,0 +1,7 @@
+INSERT INTO `form_caption_translations` 
+(`form`, `language`, `caption`) 
+VALUES
+('enter_deceased', 'en', 'Deceased Person\'s Case Entry'),
+('enter_deceased', 'nl', 'Invoer Zaak Overleden Persoon'),
+('enter_missing', 'en', 'Missing Person\'s Case Entry'),
+('enter_missing', 'nl', 'Invoer Zaak Vermiste Persoon');
