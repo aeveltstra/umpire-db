defddf1ab2eefbc613fc9fb8c010b12864306a1e
Ænðr E. Feldstraw <aev@sdf.org>
2023-12-27 22:17:03 -0500

    corrected table structure

--- model/all_tables.sql
+++ model/all_tables.sql
@@ -133,17 +133,22 @@ CREATE TABLE enums (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='All possible enumerations for all attributes';
 
 DROP TABLE IF EXISTS enum_values;
-CREATE TABLE enum_values (
-  attribute_id char(24) NOT NULL,
-  case_id bigint(20) NOT NULL,
-  enum_id varchar(24) NOT NULL,
-  PRIMARY KEY (attribute_id,case_id),
-  KEY enum_values_case_ids (case_id),
-  KEY enum_values_enum_ids (enum_id),
-  CONSTRAINT fk_attrib_id_enum_values FOREIGN KEY (attribute_id) REFERENCES attributes (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_case_id_enum_values FOREIGN KEY (case_id) REFERENCES profiles (case_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_enum_id_enum_values FOREIGN KEY (enum_id) REFERENCES enums (enum_value) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Enumerated values for enumerated attributes: a single choice from multiple. Useful for standardized nouns like hair color and units of measurement.';
+CREATE TABLE `enum_values` (
+ `at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ `attribute_id` char(24) NOT NULL,
+ `case_id` bigint(20) NOT NULL,
+ `enum_id` varchar(24) NOT NULL,
+ `user` int(10) unsigned NOT NULL,
+ PRIMARY KEY (`attribute_id`,`case_id`,`at`) USING BTREE,
+ KEY `ix_enum_values_modified` (`at`),
+ KEY `ix_enum_values_user` (`user`),
+ KEY `ix_enum_values_case_id` (`case_id`) USING BTREE,
+ KEY `ix_enum_values_enum_id` (`enum_id`) USING BTREE,
+ CONSTRAINT `fk_attrib_id_enum_values` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `fk_case_id_enum_values` FOREIGN KEY (`case_id`) REFERENCES `profiles` (`case_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `fk_enum_id_enum_values` FOREIGN KEY (`enum_id`) REFERENCES `enums` (`enum_value`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `fk_enum_values_user` FOREIGN KEY (user) REFERENCES users (seq)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Enumerated values for enumerated attributes: a single choice from multiple. Useful for standardized nouns like hair color and units of measurement.'
 
 DROP TABLE IF EXISTS form_attributes;
 CREATE TABLE form_attributes (
--- model/tbl_enum_values.sql
+++ model/tbl_enum_values.sql
@@ -1,13 +1,16 @@
-
-DROP TABLE IF EXISTS `enum_values`;
 CREATE TABLE `enum_values` (
-  `attribute_id` char(24) NOT NULL,
-  `case_id` bigint(20) NOT NULL,
-  `enum_id` varchar(24) NOT NULL,
-  PRIMARY KEY (`attribute_id`,`case_id`),
-  KEY `enum_values_case_ids` (`case_id`),
-  KEY `enum_values_enum_ids` (`enum_id`),
-  CONSTRAINT `fk_attrib_id_enum_values` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_case_id_enum_values` FOREIGN KEY (`case_id`) REFERENCES `profiles` (`case_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_enum_id_enum_values` FOREIGN KEY (`enum_id`) REFERENCES `enums` (`enum_value`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Enumerated values for enumerated attributes: a single choice from multiple. Useful for standardized nouns like hair color and units of measurement.'	
+ `at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ `attribute_id` char(24) NOT NULL,
+ `case_id` bigint(20) NOT NULL,
+ `enum_id` varchar(24) NOT NULL,
+ `user` int(10) unsigned NOT NULL,
+ PRIMARY KEY (`attribute_id`,`case_id`,`at`) USING BTREE,
+ KEY `ix_enum_values_modified` (`at`),
+ KEY `ix_enum_values_user` (`user`),
+ KEY `ix_enum_values_case_id` (`case_id`) USING BTREE,
+ KEY `ix_enum_values_enum_id` (`enum_id`) USING BTREE,
+ CONSTRAINT `fk_attrib_id_enum_values` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `fk_case_id_enum_values` FOREIGN KEY (`case_id`) REFERENCES `profiles` (`case_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `fk_enum_id_enum_values` FOREIGN KEY (`enum_id`) REFERENCES `enums` (`enum_value`) ON DELETE CASCADE ON UPDATE CASCADE
+ CONSTRAINT `fk_enum_values_user` FOREIGN KEY (user) REFERENCES users (seq)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Enumerated values for enumerated attributes: a single choice from multiple. Useful for standardized nouns like hair color and units of measurement.'
