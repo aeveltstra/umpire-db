9dd5b2790e5cb6157dd1add7fa20a7275ef930a9
Ænðr E. Feldstraw <aev@sdf.org>
2024-01-11 22:28:09 -0500

    Aligning to table change

--- model/sp_store_date.sql
+++ model/sp_store_date.sql
@@ -1,7 +1,14 @@
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS `sp_store_date`$$
-CREATE DEFINER=`van`@`10.%` PROCEDURE `sp_store_date` (IN `case_id` BIGINT(20), IN `attribute_id` CHAR(24) CHARSET utf8, IN `new_value` DATE, IN `user_email_hash` CHAR(128) CHARSET utf8)  MODIFIES SQL DATA
+CREATE DEFINER=`van`@`10.%` PROCEDURE `sp_store_date` (
+    IN `case_id` BIGINT(20), 
+    IN `attribute_id` CHAR(24) CHARSET utf8, 
+    IN `new_year` year,
+    IN `new_month` int,
+    IN `new_day` int,    
+    IN `user_email_hash` CHAR(128) CHARSET utf8
+)  MODIFIES SQL DATA
 begin
     declare user_int int(10) default null;
     select seq into user_int from users where email_hash = user_email_hash;
@@ -10,12 +17,22 @@ begin
         select seq into user_int from users where email_hash = user_email_hash;
     end if;
     if (user_int is not null) THEN
-        insert into date_values (at, attribute_id, case_id, user, value) values (
+        insert into date_values (
+            at, 
+            attribute_id, 
+            case_id, 
+            user, 
+            year,
+            month,
+            day
+        ) values (
             now(),
             attribute_id,
             case_id,
             user_int,
-            nullif(trim(new_value), '')
+            new_year,
+            new_month,
+            new_day
         );
     end if;
 END$$
