b2383c72dbe54b879680fff3189df15beea2c00a
Ænðr E. Feldstraw <aev@sdf.org>
2024-01-11 22:23:55 -0500

    Better architecture for comparison algorithms
    We want to be able to compare years and months with a variable day, depending on uncertainty. Storing the date as a date data type makes that cumbersome.

--- model/tbl_date_values.sql
+++ model/tbl_date_values.sql
@@ -3,14 +3,19 @@ DROP TABLE IF EXISTS `date_values`;
 CREATE TABLE `date_values` (
   `attribute_id` char(24) NOT NULL,
   `case_id` bigint(20) NOT NULL,
-  `value` date NOT NULL,
+  `epoch` timestamp NULL,
+  `year` year NULL,
+  `month` int NULL,
+  `day` int NULL,
   `user` int(10) unsigned NOT NULL,
   `at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`attribute_id`,`case_id`,`at`) USING BTREE,
-  KEY `value` (`value`),
+  KEY `ix_date_value` (`year`, `month`, `day`),
+  KEY `ix_date_epoch` (`epoch`),
+  KEY `ix_date_year_month` (`year`, `month`),
   KEY `ix_date_values_user` (`user`),
   KEY `ix_date_values_modified` (`at`),
   KEY `ix_date_values_case` (`case_id`) USING BTREE,
   CONSTRAINT `fk_dates_attribute` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_dates_case` FOREIGN KEY (`case_id`) REFERENCES `profiles` (`case_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Values for date attributes'	
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Values for date attributes, assuming a proleptic Gregorian calendar'	
