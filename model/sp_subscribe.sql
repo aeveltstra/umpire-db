f847af285377be16aa01e6770765bde236ca4cf1
Ænðr E. Feldstraw <aev@sdf.org>
2024-01-14 21:13:46 -0500

    Avoid inserting if exists already

--- model/sp_subscribe.sql
+++ model/sp_subscribe.sql
@@ -6,11 +6,24 @@ CREATE DEFINER=`van`@`10.%` PROCEDURE `sp_subscribe` (
     IN `email` VARCHAR(256) CHARSET utf8,
     OUT `success` BOOLEAN
 )  begin
-    insert into `subscriptions` (`case_id`, `email`) 
-        values (case_id, email);
+    declare amount_existing int default 0;
     select 0 into success;
-    select 1 into success from `subscriptions` 
+    select count(*) into amount_existing 
+        from `subscriptions` 
         where `case_id` = case_id
           and `email` = email;
+    if (0 = amount_existing) then
+        insert into `subscriptions` (`case_id`, `email`) 
+            values (case_id, email);
+        select count(*) into amount_existing 
+            from `subscriptions` 
+            where `case_id` = case_id
+              and `email` = email;
+        if (0 < amount_existing) then
+            select 1 into success;
+        end if;
+    else
+        select 1 into success;
+    end if;
 END$$
 
