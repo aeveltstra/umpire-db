54e62988eae3510fe326cc4a2f069eada77c1a42
A.E.Veltstra <aveltstra@mambro.com>
2024-07-07 15:36:52 -0400

    Returning result with expected field name

--- model/sp_may_user_reset_own_authentication.sql
+++ model/sp_may_user_reset_own_authentication.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS `sp_may_user_reset_own_authentication`$$
 CREATE PROCEDURE `sp_may_user_reset_own_authentication`(
     IN  `user_email_hash` CHAR(128) CHARSET utf8
 ) READS SQL DATA  
-    select (count(`seq`) < 5) 
+    select (count(`seq`) < 5) as is_allowed
         from `user_events`
         where `user` = (
             select `seq`
