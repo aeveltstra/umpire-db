6364e6758eba95ccb9bbb8642e512102cd11158f
A.E.Veltstra <aveltstra@mambro.com>
2024-10-10 22:47:47 -0400

    Adjusting image storing procedure to reflect added fields

--- model/sp_store_image.sql
+++ model/sp_store_image.sql
@@ -5,8 +5,12 @@ DROP PROCEDURE IF EXISTS `sp_store_image`$$
 CREATE PROCEDURE `sp_store_image`(
     IN `case_id` BIGINT(20), 
     IN `attribute_id` CHAR(24) CHARSET utf8, 
-    IN `new_value` TINYTEXT CHARSET utf8, 
-    IN `user_email_hash` CHAR(128) CHARSET utf8)
+    IN `new_url` VARCHAR(512) CHARSET utf8, 
+    IN `user_email_hash` CHAR(128) CHARSET utf8,
+    IN `alt` VARCHAR(256) CHARSET utf8,
+    IN `descr` VARCHAR(1024) CHARSET utf8,
+    IN `is_public` TINYINT UNSIGNED
+)
 begin
     declare user_int int(10) default null;
     select seq into user_int from users where email_hash = user_email_hash;
@@ -15,13 +19,16 @@ begin
         select @new_user_id into user_int;
     end if;
     if (user_int is not null) THEN
-        insert into image_values (at, attribute_id, case_id, user, value) 
+        insert into images (at, attribute_id, case_id, user, url, alt, description, is_public) 
         values (
             now(),
             attribute_id,
             case_id,
             user_int,
-            nullif(trim(new_value), '')
+            nullif(trim(new_url), ''),
+            nullif(trim(alt), ''),
+            nullif(trim(descr), ''),
+            is_public
         );
     end if;
 END$$
