635ee124defa4c22074db40f77682b9fc44f3281
A.E.Veltstra <aveltstra@mambro.com>
2024-10-11 16:10:15 -0400

    Added images to default form fields

--- data/language_codes.sql
+++ data/language_codes.sql
@@ -1,6 +1,15 @@
 TRUNCATE TABLE `language_codes`;
 INSERT INTO `language_codes` (`code`) VALUES
+('af'),
 ('de'),
 ('en'),
+('en-ZA'),
 ('fr'),
-('nl');
+('fr-CA'),
+('fr-CH'),
+('nl'),
+('es'),
+('es-CO'),
+('es-ES'),
+('es-MX'),
+('es-PR');
